49a363eee4c84180fc7d3b67c7b283afb664c800
Dakota <bazookaharwell@gmail.com>
2017-05-01 10:01:21 -1000

    Implement promisified version of getData

--- index.js
+++ index.js
@@ -9,14 +9,9 @@ function getData(index, cb) {
 // wrap getData in a promise and console log the output
 
 let getDataPromise = new Promise( (resolve, reject) => {
-  resolve( (index, cb) => {
-    let err = null;
-    if (index < 0) { err = new Error('index out of bounds'); }
-    return cb(err, data[index]);
-  });
-});
-
-console.log(getDataPromise)
+  resolve(null);
+})
+.then()
 
 // call getData three times and console log the output (use .then for the second and third call) using promises
 
@@ -24,4 +19,29 @@ console.log(getDataPromise)
 
 // create a promisified version of getData
 
+function getDataPromisified(index) {
+  return new Promise((resolve, reject) => {
+    let err = null;
+    if (index < 0) { err = new Error('index out of bounds'); }
+    if (err) {
+      reject(err);
+    } else {
+      resolve(data[index]);
+    }
+  });
+}
+
+getDataPromisified(2)
+.then( (data) => {
+  console.log(data);
+});
+
 // call the promisified version of getData with -1, console log the error in a .catch
+
+getDataPromisified(-1)
+.then( (data) => {
+
+})
+.catch( (err) => {
+  console.log(err);
+});
