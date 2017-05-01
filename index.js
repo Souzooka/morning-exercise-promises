959e30ddc6150fdc2418b045273d7bc49db4049d
Dakota <bazookaharwell@gmail.com>
2017-05-01 09:43:11 -1000

    warp getData in a promise

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ function getData(index, cb) {
 
 // wrap getData in a promise and console log the output
 
+let getDataPromise = new Promise( (resolve, reject) => {
+  resolve( (index, cb) => {
+    let err = null;
+    if (index < 0) { err = new Error('index out of bounds'); }
+    return cb(err, data[index]);
+  });
+});
+
+console.log(getDataPromise)
+
 // call getData three times and console log the output (use .then for the second and third call) using promises
 
 // create a promise within getData and use the promise to trigger the callback in getData
